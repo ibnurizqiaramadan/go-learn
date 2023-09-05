57a23fb2490916fb3c8da4eee26f3c7954569b39
pebisandi04 <pebisandi04@gmail.com>
2023-09-05 13:26:25 +0700

    update function GetElasticSearch

--- src/Api/handlers/ElasticSearch/GetElasticSearch.go
+++ src/Api/handlers/ElasticSearch/GetElasticSearch.go
@@ -1,50 +1,42 @@
 package ElasticSearch
 
 import (
+	"context"
+
 	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esutil"
 	"github.com/gofiber/fiber/v2"
+
+	"go-learning/src/Utils/ElasticSearch"
 )
 
 func GetElasticSearch(c *fiber.Ctx) error {
-	// es, err := ElasticSearch.ConnectElasticsearch()
-	// if err != nil {
-	// 	log.Fatalf("Failed to connect to Elasticsearch: %s", err)
-	// }
-
 	index := c.Query("index")
-
-	cfg := elasticsearch.Config{
-		Addresses: []string{
-			"http://194.233.95.186:9331",
-		},
-	}
+	cfg := ElasticSearch.ElasticConfig
 
 	es, err := elasticsearch.NewClient(cfg)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).SendString("Gagal terhubung ke Elasticsearch")
 	}
 
-	response, err := es.Get(index, "counter_bookmark")
+	query := map[string]interface{}{
+		"query": map[string]interface{}{
+			"match": map[string]interface{}{
+				"counter_bookmark": 1,
+			},
+		},
+	}
+	req := esutil.NewJSONReader(query)
 
-	// query := map[string]interface{}{
-	// 	"query": map[string]interface{}{
-	// 		"match": map[string]interface{}{
-	// 			"counter_bookmark": 1,
-	// 		},
-	// 	},
-	// }
+	res, err := es.Search(
+		es.Search.WithIndex(index),
+		es.Search.WithBody(req),
+		es.Search.WithContext(context.Background()),
+	)
 
-	// req := esutil.NewJSONReader(query)
-
-	// res, err := es.Search(
-	// 	es.Search.WithIndex(index),
-	// 	es.Search.WithBody(req),
-	// 	es.Search.WithContext(context.Background()),
-	// )
-
-	// if err != nil {
-	// 	return c.Status(fiber.StatusInternalServerError).SendString("Gagal melakukan pencarian data")
-	// }
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString("Gagal melakukan pencarian data")
+	}
 
 	return c.JSON(fiber.Map{
 		"statusCode": fiber.StatusOK,
@@ -52,7 +44,7 @@ func GetElasticSearch(c *fiber.Ctx) error {
 			"valid":    true,
 			"messages": "success-get-data-redis",
 			"index":    index,
-			"data":     response,
+			"data":     res,
 		},
 	})
 }
--- src/Utils/ElasticSearch/ElasticSearch.go
+++ src/Utils/ElasticSearch/ElasticSearch.go
@@ -4,22 +4,8 @@ import (
 	"github.com/elastic/go-elasticsearch/v8"
 )
 
-var cfg = elasticsearch.Config{
+var ElasticConfig = elasticsearch.Config{
 	Addresses: []string{
 		"http://194.233.95.186:9331",
 	},
-}
-
-func ConnectElasticsearch() (*elasticsearch.Client, error) {
-	cfg := elasticsearch.Config{
-		Addresses: []string{
-			"http://194.233.95.186:9331",
-		},
-	}
-
-	es, err := elasticsearch.NewClient(cfg)
-	if err != nil {
-		return nil, err
-	}
-	return es, nil
 }
