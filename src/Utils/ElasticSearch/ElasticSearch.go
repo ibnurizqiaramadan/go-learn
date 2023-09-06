ff7d95d50b8d3f1107441ee83bb323782082b22b
ibnurizqiaramadan <dadanibnu61@gmail.com>
2023-09-06 12:57:54 +0700

    implement env

--- src/Utils/ElasticSearch/ElasticSearch.go
+++ src/Utils/ElasticSearch/ElasticSearch.go
@@ -1,11 +1,13 @@
 package ElasticSearch
 
 import (
+	"os"
+
 	"github.com/elastic/go-elasticsearch/v8"
 )
 
 var ElasticConfig = elasticsearch.Config{
 	Addresses: []string{
-		"http://192.168.69.70:9331",
+		os.Getenv("ELASTICSEARCH_ADDRESS"),
 	},
 }
