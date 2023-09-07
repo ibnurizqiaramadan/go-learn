b24e8e5af551fffcb3b3533fc459600d9c99f5c7
ibnurizqiaramadan <dadanibnu61@gmail.com>
2023-09-07 10:42:07 +0700

    AuthMiddleware changes - add excludepaths function - set variable for get decoded auth token

--- src/Api/middleware/AuthMiddleware.go
+++ src/Api/middleware/AuthMiddleware.go
@@ -2,21 +2,40 @@ package AuthMiddleware
 
 import (
 	"go-learning/src/Utils/Jwt"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 )
 
+func excludePaths(path string, arrPath []string) bool {
+	matchCount := 0
+	for _, path_ := range arrPath {
+		if strings.HasSuffix(path_, "*") {
+			if strings.Contains(path, strings.TrimSuffix(path_, "*")) {
+				matchCount++
+				continue
+			}
+		}
+		if strings.Contains(path, path_) && len(path_) == len(path) {
+			matchCount++
+		}
+	}
+	return matchCount > 0
+}
+
+var AuthData any
+var IgnoreAuth []string
+
 func CheckAuth(c *fiber.Ctx) error {
-	if c.Path() == "/" {
+
+	if excludePaths(c.Path(), IgnoreAuth) {
 		return c.Next()
 	} else {
 		header := c.GetReqHeaders()
 		token := header["Authorization"]
-		_, status, err := Jwt.VerifyToken(string(token))
-		// data, status := Jwt.VerifyToken(string(token))
-
-		// log.Info(data, status)
+		data, status, err := Jwt.VerifyToken(string(token))
+		AuthData = data
 
 		if err != nil {
 			log.Error(err)
